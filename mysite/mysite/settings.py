f8fa91a06d71efdccf369492e4f605e318a2f261
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-01-02 16:46:10 +0200

    Adding images.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = "/static/"
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,4 +123,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '../static/'
+
+
