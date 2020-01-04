6658df669709accf50b98d2f44c47fde1fd98921
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-01-04 12:21:46 +0200

    Removing url static path

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,12 +5,5 @@ from django.conf import settings
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    path(r'ckeditor/', include('ckeditor_uploader.urls')),
 ]
-
-if not settings.DEBUG:
-    urlpatterns += path('',
-                        (
-                            r'^static/(?P<path>.*)$', 'django.views.static.serve',
-                            {'document_root': settings.STATIC_ROOT}),
-                        )
