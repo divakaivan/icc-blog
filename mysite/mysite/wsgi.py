1176114a1363b8b85bb4e9c61051070ff1f2468b
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-01-04 17:38:43 +0200

    Testing procfile

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+path = '/settings.py'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
 
