001bc86bee2ee8bbcb910b934de0ba7791e82e00
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-01-04 20:54:45 +0200

    Moving the db to postgresql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.postgres',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -89,8 +90,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "portfoliodb",
+        "USER": "postgres",
+        "PASSWORD": "980602Ivan",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
